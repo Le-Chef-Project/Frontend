c20c3eb49e5feb2959f84efe5ed5dbf6b232592d
Thaowpsta <thaowpstasaiid@gmail.com>
2024-07-15 03:44:20 +0300

    Screen 7

---
+++ le_chef/lib/Screens/seeAllVid.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:le_chef/Shared/customBottomNavBar.dart';
+import '../Shared/custom_app_bar.dart';
+import 'package:majesticons_flutter/majesticons_flutter.dart'; // Correct import
+
+class AllVid extends StatelessWidget {
+  const AllVid({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    bool isLocked = true; // Variable to control the visibility of the looked icon
+
+    return Scaffold(
+      appBar: CustomAppBar(title: "All Videos"),
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(color: Colors.white),
+        child: ListView(
+          padding: const EdgeInsets.all(16),
+          children: List.generate(4, (index) {
+            return Padding(
+              padding: const EdgeInsets.only(bottom: 16),
+              child: Card(
+                color: Color(0xCC888888),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                elevation: 4,
+                child: Stack(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(12),
+                      child: Image.network(
+                        "https://images.unsplash.com/reserve/bOvf94dPRxWu0u3QsPjF_tree.jpg?ixid=M3wxMjA3fDB8MXxzZWFyY2h8M3x8bmF0dXJhbHxlbnwwfHx8fDE3MjA5MjY0NjR8MA&ixlib=rb-4.0.36",
+                        width: double.infinity,
+                        height: 200,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Positioned.fill(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.black.withOpacity(0.3),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                    ),
+                    if (isLocked)
+                      Positioned(
+                        bottom: 8,
+                        right: 8,
+                        child: Icon(
+                          Icons.lock,
+                          color: Colors.white,
+                          size: 24,
+                        ),
+                      ),
+                    Positioned.fill(
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: IconButton(
+                          icon: Icon(Icons.play_arrow, size: 58, color: Colors.white),
+                          onPressed: () {
+                            // Define your play button functionality here
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          }),
+        ),
+      ),
+      bottomNavigationBar: custumBottomNavBar(),
+    );
+  }
+}
---
+++ le_chef/lib/Shared/customBottomNavBar.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class custumBottomNavBar extends StatelessWidget {
+  const custumBottomNavBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home_outlined),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.notifications_none_outlined),
+          label: 'Notifications',
+        ),
+        BottomNavigationBarItem(
+          icon: Image.network('https://i.pinimg.com/originals/13/87/be/1387be74b2c4d1c58982a7b056f8e54e.jpg', width: 30, height: 25, fit: BoxFit.cover),
+          label: 'Messages',
+        ),
+      ],
+      selectedItemColor: Color(0xFF164863),
+      unselectedItemColor: Color(0xFF164863),
+      backgroundColor: Colors.white,
+    );
+  }
+}
